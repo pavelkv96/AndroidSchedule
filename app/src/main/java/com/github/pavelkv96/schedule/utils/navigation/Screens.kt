c5cb8072d5719c687979d39c49110f19b69cbdc0
Kalinin_PV <pavelkv96@gmail.com>
2019-09-06 11:53:46 +0300

    Added empty fragments and layout for their

--- app/src/main/java/com/github/pavelkv96/schedule/utils/navigation/Screens.kt
+++ app/src/main/java/com/github/pavelkv96/schedule/utils/navigation/Screens.kt
@@ -17,7 +17,7 @@ package com.github.pavelkv96.schedule.utils.navigation
 
 import androidx.fragment.app.Fragment
 import com.github.pavelkv96.schedule.ui.flow.HomeFragment
-import com.github.pavelkv96.schedule.ui.fragments.MainFragment
+import com.github.pavelkv96.schedule.ui.fragments.*
 import ru.terrakok.cicerone.android.support.SupportAppScreen
 
 class Screens(private val key: String, private val data: Any? = null) : SupportAppScreen() {
@@ -29,7 +29,12 @@ class Screens(private val key: String, private val data: Any? = null) : SupportA
     override fun getFragment(): Fragment {
         return when (key) {
             main_flow -> HomeFragment.newInstance()
+            about_fragment -> AboutFragment.newInstance()
+            departments_fragment -> DepartmentsListFragment.newInstance()
             main_fragment -> MainFragment.newInstance()
+            settings_fragment -> SettingsFragment.newInstance()
+            student_fragment -> StudentFragment.newInstance()
+            teachers_fragment -> TeachersListFragment.newInstance()
             else -> throw NullPointerException("Not found fragment with key screen: $key")
         }
     }
@@ -37,6 +42,11 @@ class Screens(private val key: String, private val data: Any? = null) : SupportA
     companion object {
 
         const val main_flow = "home_flow"
+        const val about_fragment = "about_fragment"
+        const val departments_fragment = "departments_fragment"
         const val main_fragment = "main_fragment"
+        const val settings_fragment = "settings_fragment"
+        const val student_fragment = "student_fragment"
+        const val teachers_fragment = "teachers_fragment"
     }
 }
